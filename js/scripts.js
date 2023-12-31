35bcd94837de81b86e22deb301dbd56fd6bc977b
kabrownie <makonokevin@gmail.com>
2023-12-31 17:01:03 +0300

    lik

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,20 @@
  
 
 */
-
+$(document).ready(function(){
+    $("img").click(function(){
+    var t = $(this).attr("src");
+    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $("#myModal").modal();
+  });
+  
+  $("video").click(function(){
+    var v = $("video > source");
+    var t = v.attr("src");
+    $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
+    $("#myModal").modal();  
+  });
+  });//EOF Document.ready
 
 (function ($) {
     'use strict';
@@ -104,7 +117,7 @@
 
         $(".typing").typed({
         
-            strings: ["Hello there", "Welcome","This is Where Kabrownie's works reside"],    
+            strings: ["Hello there", "Welcome","Check out cool 3ds below"],    
             typeSpeed: 100
           });
          });
